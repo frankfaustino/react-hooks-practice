2690c7ff99dfbfbddc42523c1c07fc7fe2d549fa
frankfaustino <nawzyah@gmail.com>
2019-06-29 15:47:29 -0700

    ✨ Add counter example for useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import { useForm } from './useForm'
+
+import './App.css'
 
 const App: React.FC = () => {
+  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 })
+  const { values, handleChange, handleSubmit } = useForm(() => ({ email: '', password: '' }))
+  const submit = () => alert(`${values.email} / ${values.password}`)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{count}</div>
+        <div>{count2}</div>
+        <button onClick={e => setCount(({ count, count2 }) => ({ count: count + 1, count2 }))}>one</button>
+        <button onClick={e => setCount(({ count, count2 }) => ({ count, count2: count2 + 1 }))}>two</button>
+        <br />
+        <input name="email" onChange={handleChange} value={values.email} />
+        <input name="password" onChange={handleChange} type="password" value={values.password} />
+        <button onClick={handleSubmit(submit)}>submit</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
