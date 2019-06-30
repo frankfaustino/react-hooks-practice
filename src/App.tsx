d9a75a9c305d8e7ec62222b91dca513d3c59b656
frankfaustino <nawzyah@gmail.com>
2019-06-29 19:25:27 -0700

    ✨ useReducer example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Sup from './Sup'
+import Todos from './Todos'
 import useForm from './useForm'
 import useFetch from './useFetch'
 
@@ -8,7 +9,8 @@ import './App.css'
 
 const App: React.FC = () => {
   // useState example showing the two states of a counter
-  const [{ count, count2 }, setCount] = useState({ count: 0, count2: 20 })
+  const [count, setCount] = useState(JSON.parse(localStorage.getItem('count') || '' + 1))
+  const [count2, setCount2] = useState(20)
 
   // Typical use case for a custom hook, the form
   const { values, handleChange, handleSubmit } = useForm(() => ({ email: '', password: '' }))
@@ -21,13 +23,21 @@ const App: React.FC = () => {
   // Custom hook using useEffect and useState
   const [data, loading] = useFetch(`http://numbersapi.com/${count}/trivia`)
 
+  // useEffect w/ localStorage
+  useEffect(() => {
+    localStorage.setItem('count', count)
+    return () => {
+      localStorage.removeItem('count')
+    }
+  }, [count])
+
   return (
     <div className="App">
       <header className="App-header">
         <div>{loading ? '' : String(data)}</div>
-        <button onClick={e => setCount(({ count, count2 }) => ({ count: count + 1, count2 }))}>one</button>
+        <button onClick={() => setCount((c: number) => c + 1)}>one</button>
         <div>{count2}</div>
-        <button onClick={e => setCount(({ count, count2 }) => ({ count, count2: count2 + 1 }))}>two</button>
+        <button onClick={() => setCount2((c: number) => c + 1)}>two</button>
         <br />
         <input name="email" onChange={handleChange} value={values.email} />
         <input name="password" onChange={handleChange} type="password" value={values.password} />
@@ -35,6 +45,8 @@ const App: React.FC = () => {
         <br />
         <button onClick={toggleShowSup}>sup?</button>
         {showSup && <Sup />}
+        <br />
+        <Todos />
       </header>
     </div>
   )
---
+++ src/Todos.tsx
@@ -0,0 +1,53 @@
+import React, { ChangeEvent, MouseEvent, useReducer, useState } from 'react'
+
+// https://www.sumologic.com/blog/usereducer-react-hook-typescript/
+
+type Todos = {
+  text: string
+  completed: boolean
+}
+
+type State = {
+  todos: Todos[]
+  todoCount: number
+}
+
+type Action =
+  | { type: 'add', text: string }
+  | { type: 'toggle', index: number }
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'add':
+      return { todos: [...state.todos, { text: action.text, completed: false }], todoCount: state.todoCount + 1 }
+    case 'toggle':
+      return { ...state, todos: state.todos.map((t, i) => i === action.index ? { ...t, completed: !t.completed } : t) }
+    default:
+      return state
+  }
+}
+
+const Todos: React.FC = () => {
+  const initialState: State = { todos: [], todoCount: 0 }
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const [text, setText] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)
+
+  const handleClick = (index: number) => (_: MouseEvent<HTMLDivElement>) => dispatch({ type: 'toggle', index })
+
+  return (
+    <form onSubmit={e => {
+      e.preventDefault()
+      dispatch({ type: 'add', text })
+      setText('')
+    }}>
+      <input value={text} onChange={handleChange} />
+      {state.todos.map(({ completed, text }, i) => (
+        <div key={text + i} onClick={handleClick(i)} style={{ textDecoration: completed ? 'line-through' : '' }}>{text}</div>)
+      )}
+    </form>
+  )
+}
+
+export default Todos
